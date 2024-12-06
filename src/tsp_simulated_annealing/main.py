3c099387eff1596bc769816d1ca0709f669cb5de
TikaBennekum <107108939+TikaBennekum@users.noreply.github.com>
2024-12-06 12:28:55 +0100

    calculating distance 2 points

--- src/tsp_simulated_annealing/main.py
+++ src/tsp_simulated_annealing/main.py
@@ -1,3 +1,4 @@
+import math
 from pathlib import Path
 
 small_path = Path("../../tsp_problems/eil51.tsp.txt")
@@ -11,19 +12,21 @@ data_large = large_path.read_text().split("\n")[6:][:-2]
 
 class Node:
     def __init__(self, id, x, y):
-        self.id = id
-        self.x = x
-        self.y = y
+        self.id = int(id)
+        self.x = float(x)
+        self.y = float(y)
 
 
 def data_to_nodes(data):
     nodes = []
+    coordinates = [None]
     for elem in data:
         info = elem.split(" ")
         info = " ".join(info).split()
         id, x, y = info
         nodes.append(Node(id, x, y))
-    return nodes
+        coordinates.append((float(x), float(y)))
+    return (nodes, coordinates)
 
 
 def show_data(nodes):
@@ -31,5 +34,11 @@ def show_data(nodes):
         print(f"id: {node.id}, x: {node.x}, y: {node.y}")
 
 
-nodes = data_to_nodes(data_small)
-show_data(nodes)
+def distance_two_nodes(id1, id2, coordinates):
+    x1, y1 = coordinates[id1]
+    x2, y2 = coordinates[id2]
+    print(x1, y1, x2, y2)
+    return math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+
+
+nodes, coordinates = data_to_nodes(data_small)
