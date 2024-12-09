a43ad4f121ebedf695f4dc0595bc80b06914095b
TikaBennekum <107108939+TikaBennekum@users.noreply.github.com>
2024-12-09 17:41:52 +0100

    if __name__=='__main__':

--- experiments/experiment_0.py
+++ experiments/experiment_0.py
@@ -6,14 +6,14 @@ from tsp_simulated_annealing.cooling_schedules import (
 )
 from tsp_simulated_annealing.main import main_algorithm
 
-cooling_schedule = partial(inverse_log_cooling, a=5, b=1)
+if __name__ == "__main__":
+    cooling_schedule = partial(inverse_log_cooling, a=5, b=1)
 
+    test_path_2 = Path("../tsp_problems/eil51.tsp.txt")
+    print(test_path_2)
 
-test_path_2 = Path("../tsp_problems/eil51.tsp.txt")
-print(test_path_2)
+    data_small = test_path_2.read_text().split("\n")[6:][:-2]
 
-data_small = test_path_2.read_text().split("\n")[6:][:-2]
+    solution, dist = main_algorithm(data_small, 100, cooling_schedule, 20)
 
-solution, dist = main_algorithm(data_small, 100, cooling_schedule, 20)
-
-print(f"solution: {solution}, distance {dist} ")
+    print(f"solution: {solution}, distance {dist} ")
