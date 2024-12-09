bfaefc55f615e2f9788c1802281cbdb1db31d493
PetrChalupsky <petr.chalupsky@student.uva.nl>
2024-12-09 17:12:44 +0100

    Fixed rng

--- experiments/experiment_0.py
+++ experiments/experiment_0.py
@@ -1,6 +1,8 @@
 from functools import partial
 from pathlib import Path
 
+import numpy as np
+
 from tsp_simulated_annealing.cooling_schedules import (
     inverse_log_cooling,
 )
@@ -8,12 +10,12 @@ from tsp_simulated_annealing.main import main_algorithm
 
 cooling_schedule = partial(inverse_log_cooling, a=5, b=1)
 
-
+rng = np.random.default_rng(42)
 test_path_2 = Path("../tsp_problems/eil51.tsp.txt")
-print(test_path_2)
+
 
 data_small = test_path_2.read_text().split("\n")[6:][:-2]
 
-solution, dist = main_algorithm(data_small, 100, cooling_schedule, 20)
+solution, dist = main_algorithm(data_small, 100, cooling_schedule, 20, rng)
 
 print(f"solution: {solution}, distance {dist} ")
