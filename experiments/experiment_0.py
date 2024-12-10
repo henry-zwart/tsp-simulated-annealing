3c6001bbf7f2d0365353268c500a5e2cf5bfa11c
Henry Zwart <henry.zwart@dragonfly.co.nz>
2024-12-10 15:12:14 +0100

    fix experiment_0

--- experiments/experiment_0.py
+++ experiments/experiment_0.py
@@ -23,8 +23,8 @@ if __name__ == "__main__":
     cooling_schedule = partial(inverse_log_cooling, a=5, b=1)
 
     rng = np.random.default_rng(42)
-    small_path = Path("../tsp_problems/eil51.tsp.txt")
-    data_small = small_path.read_text().split("\n")[6:][:-2]
+    small_path = Path("tsp_problems/eil51.tsp.txt")
+    data_small = small_path.read_text().split("\n")[:-1]  # [6:][:-2]
 
     solution, dist = main_algorithm(
         data_small, 100, cooling_schedule, 20, rng, acceptance
